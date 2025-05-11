74689354546216b8b67c61c9b4401eb8bea7826e
Rahul T <developer@oceanz.in>
2025-05-11 13:20:17 +0530

    Moved the application into root

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from "drizzle-kit";
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
+config({
+  path: '.env.local',
+});
 
 export default defineConfig({
-  out: "./migrations",
-  schema: "./shared/schema.ts",
-  dialect: "postgresql",
+  schema: './lib/db/schema.ts',
+  out: './lib/db/migrations',
+  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    // biome-ignore lint: Forbidden non-null assertion.
+    url: process.env.POSTGRES_URL!,
   },
 });
